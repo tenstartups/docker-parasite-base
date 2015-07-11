52ae8f66155c94f22ec5e68d169f08306a87ba86
Marc Lennox <marc.lennox@gmail.com>
2015-07-11 16:51:06 -0400

    Changed the way docker image update checking is done

--- 10-common/init.d/12-docker-images.sh
+++ 10-common/init.d/12-docker-images.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -e
 
-# Set environment variables
-DOCKER_IMAGE_NAME_REGEX="^\s*DOCKER_IMAGE_([_A-Z]+)=(.+)\s*$"
-
-echo "Pre-pulling all required docker images"
-
 # Pull all docker images at the start to ensure a smoother initialization
-while read -r docker_image_name ; do
-  /12factor/bin/docker-check-pull $docker_image_name
-done < <(env | grep -E "${DOCKER_IMAGE_NAME_REGEX}" | sed -E "s/${DOCKER_IMAGE_NAME_REGEX}/\1/")
-
+echo "Pre-pulling all required docker images"
+systemctl start docker-check-image-update.timer &
 echo "Finished pre-pulling all required docker images"
