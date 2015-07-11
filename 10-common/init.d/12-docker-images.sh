aa2f6f0859ea756f7afdd4aa05eedb9f5a37a7a9
Marc Lennox <marc.lennox@gmail.com>
2015-07-11 15:55:38 -0400

    Cleanup

--- 10-common/init.d/12-docker-images.sh
+++ 10-common/init.d/12-docker-images.sh
@@ -4,7 +4,11 @@ set -e
 # Set environment variables
 DOCKER_IMAGE_NAME_REGEX="^\s*DOCKER_IMAGE_([_A-Z]+)=(.+)\s*$"
 
+echo "Pre-pulling all required docker images"
+
 # Pull all docker images at the start to ensure a smoother initialization
 while read -r docker_image_name ; do
-  docker pull $docker_image_name
+  /12factor/bin/docker-check-pull $docker_image_name
 done < <(env | grep -E "${DOCKER_IMAGE_NAME_REGEX}" | sed -E "s/${DOCKER_IMAGE_NAME_REGEX}/\1/")
+
+echo "Finished pre-pulling all required docker images"
