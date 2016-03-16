7001ebe392d6b9f804bb31d2081215079d2aeea9
Marc Lennox <marc.lennox@gmail.com>
2016-03-16 18:30:26 -0400

    Changed ssl mount to be read only

--- host/script/send-notification.sh
+++ host/script/send-notification.sh
@@ -1,4 +1,5 @@
 #!/bin/bash +x
+set -e
 
 # Prevent re-entry into this script
 if [ "${SEND_NOTIFICATION_ENTRY_COUNT:-0}" -gt 0 ]; then
@@ -12,25 +13,26 @@ NOTIFIER_SENDER=$(hostname) # This assumes that /etc/hostname has the FQDN
 NOTIFIER_SENDER=$(IFS=. read host domain <<<"${NOTIFIER_SENDER}" && echo ${host})
 
 # Call the notifier with our without an attachment
-/opt/bin/docker-check-pull "${DOCKER_IMAGE_NOTIFIER}"
-if [ -z "${FILE_ATTACHMENT}" ]; then
-  /usr/bin/docker run --rm \
-    -v /etc/ssl:/etc/ssl \
-    --env-file="<%= getenv!(:config_directory) %>/env/docker.env" \
-    -e NOTIFIER_SENDER=${NOTIFIER_SENDER} \
-    -e MESSAGE="${MESSAGE}" \
-    --hostname=notifier.${DOCKER_HOSTNAME_FULL} \
-    ${DOCKER_IMAGE_NOTIFIER} \
-    "$@"
-else
-  /usr/bin/docker run --rm \
-    -v /etc/ssl:/etc/ssl \
-    --env-file="<%= getenv!(:config_directory) %>/env/docker.env" \
-    -v "${FILE_ATTACHMENT}:/tmp/$(basename ${FILE_ATTACHMENT}):ro" \
-    -e NOTIFIER_SENDER=${NOTIFIER_SENDER} \
-    -e MESSAGE="${MESSAGE}" \
-    -e FILE_ATTACHMENT="$(basename ${FILE_ATTACHMENT})" \
-    --hostname=notifier.${DOCKER_HOSTNAME_FULL} \
-    ${DOCKER_IMAGE_NOTIFIER} \
-    "$@"
-fi
+{
+  /opt/bin/docker-check-pull "${DOCKER_IMAGE_NOTIFIER}"
+  if [ -z "${FILE_ATTACHMENT}" ]; then
+    /usr/bin/docker run --rm \
+      --env-file="<%= getenv!(:config_directory) %>/env/docker.env" \
+      -e NOTIFIER_SENDER=${NOTIFIER_SENDER} \
+      -e MESSAGE="${MESSAGE}" \
+      --hostname=notifier.${DOCKER_HOSTNAME_FULL} \
+      ${DOCKER_IMAGE_NOTIFIER} \
+      "$@"
+  else
+    /usr/bin/docker run --rm \
+      --env-file="<%= getenv!(:config_directory) %>/env/docker.env" \
+      -v "${FILE_ATTACHMENT}:/tmp/$(basename ${FILE_ATTACHMENT}):ro" \
+      -e NOTIFIER_SENDER=${NOTIFIER_SENDER} \
+      -e MESSAGE="${MESSAGE}" \
+      -e FILE_ATTACHMENT="$(basename ${FILE_ATTACHMENT})" \
+      --hostname=notifier.${DOCKER_HOSTNAME_FULL} \
+      ${DOCKER_IMAGE_NOTIFIER} \
+      "$@"
+  fi
+
+} || true
