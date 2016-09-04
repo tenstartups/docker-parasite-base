90fd4009d56d0bc9ac7ff57e76eb88c07eee876a
Marc Lennox <marc.lennox@gmail.com>
2016-09-04 13:31:22 -0400

    Fixed a script problem

--- host/script/docker-wait-container.sh
+++ host/script/docker-wait-container.sh
@@ -12,24 +12,27 @@ if [ -z "${DOCKER_CONTAINER_NAME}" ]; then
   exit 1
 fi
 
+exit_success() {
+  # Notify success and exit
+  echo "Container ${DOCKER_CONTAINER_NAME} is running."
+  /opt/bin/send-notification success "Container \`${DOCKER_CONTAINER_NAME}\` is running."
+  exit 0
+}
+
+exit_failure() {
+  # Notify failure and exit
+  echo >&2 "Timed-out waiting for container ${DOCKER_CONTAINER_NAME} to start."
+  /opt/bin/send-notification error "Timed-out waiting for container  \`${DOCKER_CONTAINER_NAME}\` to start."
+  exit 1
+}
+
 test_number=0
 next_test_wait=1
-while [[ ${test_number} < ${MAX_CHECKS} ]]; do
+while [[ "${test_number}" -lt "${MAX_CHECKS}" ]]; do
   test_number=$(( test_number + 1 ))
-  CONTAINER_RUNNING=$(docker inspect --type container --format "{{.State.Running}}" ${DOCKER_CONTAINER_NAME} 2>/dev/null || true)
-  if [ ${test_number} = ${MAX_CHECKS} ] || [ "${CONTAINER_RUNNING}" = "true" ]; then break; fi
+  [ "$(docker inspect --type container --format "{{.State.Running}}" ${DOCKER_CONTAINER_NAME} 2>/dev/null || true)" = "true" ] && exit_success
   echo "Waiting for ${DOCKER_CONTAINER_NAME} container to start..."
   sleep ${next_test_wait}
   next_test_wait=$(( next_test_wait * 2 ))
 done
-
-# Exit based on whether the container is running or not
-if [ "${CONTAINER_RUNNING}" = "true" ]; then
-  echo "Container ${DOCKER_CONTAINER_NAME} is running."
-  /opt/bin/send-notification success "Container \`${DOCKER_CONTAINER_NAME}\` is running."
-  exit 0
-else
-  echo >&2 "Timed-out waiting for container ${DOCKER_CONTAINER_NAME} to start."
-  /opt/bin/send-notification error "Timed-out waiting for container  \`${DOCKER_CONTAINER_NAME}\` to start."
-  exit 1
-fi
+exit_failure
