601bdb694108e7f507f34278da86324339f4506a
Marc Lennox <marc.lennox@gmail.com>
2018-02-04 14:45:13 -0500

    Removed etcd

--- files/host/script/coreos/etcd-config-prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cp -n \
-  "<%= getenv!(:parasite_config_directory) %>/env/etcd-common.env" \
-  "<%= getenv!(:parasite_config_directory) %>/env/etcd.env"
--- files/host/script/coreos/etcd-data-reset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Clear the etcd data directory (destructive)
-/usr/bin/docker run --rm \
-  -v <%= getenv!(:parasite_data_docker_volume) %>-etcd:/var/lib/etcd \
-  tenstartups/alpine:<%= choose!(:parasite_os, coreos: 'latest', hypriotos: 'armhf') %> \
-  sh -c "cd /var/lib/etcd && rm -rfv proxy && rm -rfv member"
